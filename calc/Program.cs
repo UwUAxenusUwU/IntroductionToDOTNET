97df774e80ad917ff4bc04358204d314b5d09101
Axenus <platon-gamer@mail.ru>
2024-06-19 15:32:10 +0500

    calc added

---
+++ calc/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    internal class Program
+    {
+        static void calc()
+        {
+            Console.WriteLine("Введите числа: ");
+            double a = 0.0, b = 0.0, c = 0.0;
+            char act;
+            do
+            {
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Выберите операцию (+, -, *, /):");
+                act = Convert.ToChar(Console.ReadLine());
+                switch (act)
+                {
+                    case '+':
+                        c = a + b;
+                        Console.WriteLine($"{a} {act} {b} = {c}");
+
+                        break;
+                    case '-':
+                        c = a - b;
+                        Console.WriteLine($"{a} {act} {b} = {c}");
+                        act = ' ';
+                        break;
+                    case '*':
+                        c = a * b;
+                        Console.WriteLine($"{a} {act} {b} = {c}");
+                        act = ' ';
+                        break;
+                    case '/':
+                        c = a / b;
+                        Console.WriteLine($"{a} {act} {b} = {c}");
+                        act = ' ';
+                        break;
+                    default:
+                        Console.WriteLine("Такой операции нет");
+                        break;
+                }
+            } while (act != Convert.ToChar('e'));                          //выход
+
+        }
+        static void Main(string[] args)
+        {
+             calc();
+        }
+    }
+}
