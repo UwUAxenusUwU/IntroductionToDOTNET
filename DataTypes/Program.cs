00c6617d4d0a666661937eee9720bc3a30c853ad
Axenus <platon-gamer@mail.ru>
2024-06-19 05:30:09 +0500

    cube driver added

---
+++ DataTypes/Program.cs
@@ -0,0 +1,32 @@
+﻿//#define INTEGRAL_TYPES
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static readonly string delimiter = "\n--------------------------------------------------------\n";
+        static void Main(string[] args)
+        {
+#if INTEGRAL_TYPES
+            Console.WriteLine("int");
+            Console.WriteLine($"Занимает {sizeof(short)} байта");
+            Console.WriteLine($"Диапозон принимаемых значений: {short.MinValue} .... {short.MaxValue}");
+            Console.WriteLine(delimiter);
+            Console.WriteLine("int");
+            Console.WriteLine($"Занимает {sizeof(ushort)} байта");
+            Console.WriteLine($"Диапозон принимаемых значений: {ushort.MinValue} .... {ushort.MaxValue}");
+            Console.WriteLine(delimiter);
+            Console.WriteLine("int");
+            Console.WriteLine($"Занимает {sizeof(uint)} байта");
+            Console.WriteLine($"Диапозон принимаемых значений: {uint.MinValue} .... {uint.MaxValue}");
+            Console.WriteLine(delimiter); 
+#endif
+
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        static void move()
+        {
+            ConsoleKeyInfo key;                 //хотел сделать по красоте, нашёл такое решение считывания упарвления
+                int x = 0, y = 0;                                           //позиция
+            do
+            {
+
+
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        System.Console.Clear();
+                        y--;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('■');
+                        break;
+                    case ConsoleKey.DownArrow:
+                        System.Console.Clear();
+                        y++;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('■');
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        System.Console.Clear();
+                        x--;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('■');
+                        break;
+                    case ConsoleKey.RightArrow:
+                        System.Console.Clear();
+                        x++;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write('■');
+                        break;
+                    default:
+                        Console.Beep();                     //пищит если нажать не ту клавишу
+                        break;
+                }
+            }
+            while (key.Key != ConsoleKey.Escape);
+            System.Console.Clear();
+            Console.WriteLine("Game over");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write('■');
+            move();
+        }
+    }
+}
